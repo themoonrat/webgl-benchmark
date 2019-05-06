790c17e118d2772603bdfdabed456fa0902b7328
Dave Moore <themoonrat@gmail.com>
2019-05-07 00:33:27 +0100

    Use official PixiJS v5 release

--- src/js/pixi/load.js
+++ src/js/pixi/load.js
@@ -9,7 +9,7 @@ export default function loadPixi(stats, gui) {
 	const versions = [
 		"v3.0.11",
 		"v4.0.3", "v4.1.1", "v4.2.3", "v4.3.5", "v4.4.4", "v4.5.6", "v4.6.2", "v4.7.3", "v4.8.7",
-		"v5.0.0-rc.3",
+		"v5.0.1",
 		"dev"
 	].reverse();
 
