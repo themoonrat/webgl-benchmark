2994cdfc9aeade3144b5145ed7446e0fc8247ebe
Dave Moore <dave.moore@inseinc.com>
2020-02-05 14:22:14 +0000

    Updated latest versions Updated package dependencies

--- src/js/pixi/load.js
+++ src/js/pixi/load.js
@@ -9,7 +9,7 @@ export default function loadPixi(stats, gui) {
 	const versions = [
 		"v3.0.11",
 		"v4.0.3", "v4.1.1", "v4.2.3", "v4.3.5", "v4.4.4", "v4.5.6", "v4.6.2", "v4.7.3", "v4.8.8",
-		"v5.0.4", "v5.1.5", "v5.2.0",
+		"v5.0.4", "v5.1.5", "v5.2.1",
 		"dev"
 	].reverse();
 
